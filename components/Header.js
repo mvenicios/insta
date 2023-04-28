9183c75a594c4cee923763906df7b28baea51fe2
mauro <mvenicios@bol.com.br>
2023-04-28 12:12:03 -0300

    add search bar to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import VidaSnap from "../src/app/images/vidasnaplogo.png";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Header() {
   return (
@@ -24,10 +25,20 @@ export default function Header() {
             className="object-contain"
           />
         </div>
+        {/*Middle*/}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Procurar..."
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/*Right*/}
         <h1>Right side</h1>
       </div>
-      {/*Middle*/}
-      {/*Right*/}
     </>
   );
 }
