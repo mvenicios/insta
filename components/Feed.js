080a87043967c64d25931eabe9adaababd980fe2
mauro <mvenicios@bol.com.br>
2023-04-28 20:45:03 -0300

    create the min profile component

--- components/Feed.js
+++ components/Feed.js
@@ -1,3 +1,4 @@
+import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
 
@@ -11,8 +12,11 @@ export default function Feed() {
         <Posts />
       </section>
       <section className="hidden md:inline-grid md:col-span-1">
-        {/*Mini profile*/}
-        {/*Sugestions*/}
+        <div className="fixed w-[380px]">
+          {/*Mini profile*/}
+          <MiniProfile />
+          {/*Sugestions*/}
+        </div>
       </section>
     </main>
   );
